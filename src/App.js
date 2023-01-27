27d1c07c3ff38036ffefa65daa3bfdfec77bcd23
shaikat17 <shaikatpal56@gmail.com>
2023-01-27 23:16:57 +0600

    Project Completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import { useState } from "react";
-
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 
 function App() {
-  const [markDown, setMarkDown] = useState('## markdown preview')
+  const [markDown, setMarkDown] = useState("## markdown preview");
   return (
-    <h1>Markdown Preview React</h1>
+    <main>
+      <section className="markdown">
+        <textarea
+          class="input"
+          value={markDown}
+          onChange={(e) => setMarkDown(e.target.value)}
+        ></textarea>
+        <article className="result">
+          <ReactMarkdown>{markDown}</ReactMarkdown>
+        </article>
+      </section>
+    </main>
   );
 }
 
